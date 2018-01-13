2274ff7932992404f5d704b6a846cd834aa93b6a
Ian Dewsbury <ianmichaeld@gmail.com>
2018-01-13 18:37:41 +0000

    automatically backed up by learn

--- closure.js
+++ closure.js
@@ -1,6 +1,6 @@
 function parent() {
   var innerVariable = "Go to your room!";
-  console.log(inaccessible)
+  // console.log(inaccessible)
   return function childe() {
     var inaccessible = "**sneaks out the bedroom window**";
     return innerVariable;
@@ -11,6 +11,6 @@ var outerScope = parent;
 
 var innerScope = outerScope();
 
-innerScope();
+outerScope();
 
 
